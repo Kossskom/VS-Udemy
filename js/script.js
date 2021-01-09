f84d6ab9ad887f8ee409efca113e6181e45e9d9f
Konstantin <ya.kom345@yandex.ru>
2021-01-09 19:02:39 +0300

    First commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+
+// alert('Hello');
+
+// const result = confirm("Are you here?");
+
+// console.log(result);
+ 
+// const answers = [];
+
+// answers[0] = prompt("Как ваше имя?");
+// answers[1] = prompt("возраст?");
+// answers[2] = prompt("Как вашыве имя?");
+
+// console.log(answers);
+const catrgoty = "tous";
+
+console.log(`http://somesa/${catrgoty}/5`);
