1099fbe6429dffc0130d36ac37894f5a85f57f23
Konstantin <ya.kom345@yandex.ru>
2021-01-11 20:49:21 +0300

    2 task

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-const numberOfFilms = +prompt("Сколько фильмов вы уже смотрели?","");
+const numberOfFilms = +prompt("Сколько фильмов вы уже смотрели?", "");
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -7,15 +7,37 @@ const personalMovieDB = {
     genres: [],
     privat: false
 };
+if (personalMovieDB.count < 10){
+    alert('Просмотренно довольно мало фильмов');
+}else if(personalMovieDB.count >= 10 && personalMovieDB.count <= 30){
+    alert('Вы класический зритель');
+}else if(personalMovieDB.count >= 30){
+    alert('Вы киноман');
+}else {
+    alert('Произошла ошибка');
+}
 
-const lastFilm1 = prompt('Один из последних просмотренных фильмов?');
-const estimate1 = +prompt("На сколько оцените его?");
-const lastFilm2 = prompt('Один из последних просмотренных фильмов?');
-const estimate2 = +prompt("На сколько оцените его?");
+let lastFilm = [];
+let rating = [];
 
-personalMovieDB.movies[lastFilm1] = estimate1;
-personalMovieDB.movies[lastFilm2] = estimate2;
+for (let i = 0; i <= 1; i++) {
+    lastFilm = prompt('Один из последних просмотренных фильмов?', '');
+    if (lastFilm != "" && lastFilm != null && lastFilm.length < 50) {
+        for (let i = 0; i <= 0; i++){
+            rating = +prompt("На сколько оцените его?", '');
+            if (rating != 0 && rating != null && rating == Number(rating)) {
+                personalMovieDB.movies[lastFilm] = rating;
+                console.log(rating);
+            }else{
+                --i;
+            }
+        }
+    } else {
+        console.log('error');
+        --i;
+    }
+}
+
+
 
 console.log(personalMovieDB);
-
-
